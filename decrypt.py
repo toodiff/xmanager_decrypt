afc4b80cb4d1d48b4271cfef19afaf2a6f636d0f
toodiff <nettest@protonmail.com>
2020-04-18 15:55:41 +0800

    decrypt xmanager

--- decrypt.py
+++ decrypt.py
@@ -1,88 +1,217 @@
 # -*- coding: utf-8 -*-
-# python3.7
+# python >= 3.7
 # supported xmanager version <5.1, 5.1, 5.2, 6
 
 import os
 import argparse
-import base64
 import configparser
+import unicodedata
 
 from win32api import GetComputerName, GetUserName
 from win32security import LookupAccountName, ConvertSidToStringSid
-from Cryptodome.Hash import SHA256
+from base64 import b64encode, b64decode
+from Cryptodome.Hash import MD5, SHA256
 from Cryptodome.Cipher import ARC4
 
-def decrypt_string(a1, a2):
-    v1 = base64.b64decode(a2)
-    v3 = ARC4.new(SHA256.new(a1.encode('ascii')).digest()).decrypt(v1[:len(v1) - 0x20])
-    if SHA256.new(v3).digest() == v1[-32:]:
-        return v3.decode('ascii')
+USERNAME   = GetUserName()
+MASTER_PWD = None
+SID        = ConvertSidToStringSid(LookupAccountName(GetComputerName(), GetUserName())[0])
+IS_XSH     = True
+VERSION    = '5.2'
+KEY        = os.path.join(os.environ["USERPROFILE"], r"Documents\NetSarang\Xshell\Sessions")
+IS_DECRYPT = True
+
+def getCipherKey():
+    if not is_number(VERSION):
+        raise ValueError('Invalid argument: --Version')
+
+    ver = float(VERSION)
+    if 0 < ver and ver < 5.1:
+        if IS_XSH:
+            return MD5.new(b'!X@s#h$e%l^l&').digest()
+        else:
+            return MD5.new(b'!X@s#c$e%l^l&').digest()
+    elif 5.1 <= ver and ver <= 5.2:
+        return SHA256.new(SID.encode()).digest()
+    elif 5.2 < ver:
+        if MASTER_PWD == None:
+            return SHA256.new((USERNAME + SID).encode()).digest()
+        else:
+            return SHA256.new(MASTER_PWD.encode()).digest()
     else:
-        return None
+        raise ValueError('Invalid argument: --Version')
+
+def encrypt_string(password_string, need_return=False):
+    if not is_number(VERSION):
+        raise ValueError('Invalid argument: --Version')
+
+    ver = float(VERSION)
+
+    Cipher = ARC4.new(getCipherKey())
+    if ver < 5.1:
+        en_password = b64encode(Cipher.encrypt(password_string.encode())).decode()
+    else:
+        checksum = SHA256.new(password_string.encode()).digest()
+        ciphertext = Cipher.encrypt(password_string.encode())
+        en_password = b64encode(ciphertext + checksum).decode()
+    if need_return:
+        return en_password
+    else:
+        print('%-20s : %s' % ('Version', VERSION))
+        print('%-20s : %s' % ('UserName', USERNAME))
+        print('%-20s : %s' % ('Password', password_string))
+        print('%-20s : %s' % ('Encrypted Password', en_password))
+
+def decrypt_string(password_string, need_return=False):
+    if not is_number(VERSION):
+        raise ValueError('Invalid argument: --Version')
+
+    ver = float(VERSION)
+
+    Cipher = ARC4.new(getCipherKey())
+
+    try:
+        if ver < 5.1:
+            de_password = Cipher.decrypt(b64decode(password_string)).decode()
+        else:
+            data = b64decode(password_string)
+            ciphertext, checksum = data[:-SHA256.digest_size], data[-SHA256.digest_size:]
+            plaintext = Cipher.decrypt(ciphertext)
+            if SHA256.new(plaintext).digest() != checksum:
+                raise ValueError('Cannot decrypt string. The key is wrong!')
+            de_password = plaintext.decode('ascii')
+        if need_return:
+            return de_password
+        else:
+            print('%-20s : %s' % ('Version', VERSION))
+            print('%-20s : %s' % ('UserName', USERNAME))
+            print('%-20s : %s' % ('Password', password_string))
+            print('%-20s : %s' % ('Decrypted Password', de_password))
+
+    except Exception as e:
+        print(f"Password is invalid")
+
+def decrypt_file(filepath: str = ''):
+    if not os.path.isfile(filepath):
+        print(f"{filepath:=^100}\nError: No file")
+        return
+
+    file = os.path.basename(os.path.realpath(filepath))
+
+    if file.endswith(".xsh") or file.endswith(".xfp"):
+        cfg = configparser.ConfigParser()
+        try:
+            cfg.read(filepath)
+        except UnicodeDecodeError:
+            cfg.read(filepath, encoding="utf-16")
+
+        try:
+            if file.endswith(".xsh"):
+                host = cfg["CONNECTION"]["Host"]
+                port = cfg["CONNECTION"]["Port"]
+                username = cfg["CONNECTION:AUTHENTICATION"]["UserName"]
+                password = cfg["CONNECTION:AUTHENTICATION"]["Password"]
+                version = cfg["SessionInfo"]["Version"]
+
+                de_password = decrypt_string(password, True)
+            else:
+                host = cfg["Connection"]["Host"]
+                port = cfg["Connection"]["Port"]
+                username = cfg["Connection"]["UserName"]
+                password = cfg["Connection"]["Password"]
+                version = cfg["SessionInfo"]["Version"]
+
+                de_password = decrypt_string(password, True)
+
+            print(f"{filepath:=^100}")
+            print('%-20s : %s' % ('Host', host))
+            print('%-20s : %s' % ('Port', port))
+            print('%-20s : %s' % ('Version', version))
+            print('%-20s : %s' % ('UserName', username))
+            print('%-20s : %s' % ('Password', de_password))
+            print('%-20s : %s' % ('Encrypted Password', password))
+        except Exception as e:
+            print(f"{filepath:=^100}\nError:{e}")
 
 def decrypt_dir():
-    for root, dirs, files in os.walk(args.path):
+    for root, dirs, files in os.walk(KEY):
         for f in files:
-            if f.endswith(".xsh") or f.endswith(".xfp"):
-                filepath = os.path.join(root, f)
-                cfg = configparser.ConfigParser()
-                try:
-                    cfg.read(filepath)
-                except UnicodeDecodeError:
-                    cfg.read(filepath, encoding="utf-16")
+            decrypt_file(os.path.join(root, f))
 
-                try:
-                    if f.endswith(".xsh"):
-                        host = cfg["CONNECTION"]["Host"]
-                        port = cfg["CONNECTION"]["Port"]
-                        username = cfg["CONNECTION:AUTHENTICATION"]["UserName"]
-                        password = cfg["CONNECTION:AUTHENTICATION"]["Password"]
-                        version = cfg["SessionInfo"]["Version"]
+def setDefaultSessionDirByVer():
+    if not is_number(VERSION):
+        return
+    ver = float(VERSION)
+    dir = 'Xshell' if IS_XSH else 'Xftp';
+    global KEY
+    if ver < 6:
+        KEY = os.path.join(os.environ["USERPROFILE"], r"Documents\NetSarang\%s\Sessions" % dir)
+    elif ver == 6:
+        KEY = os.path.join(os.environ["USERPROFILE"], r"Documents\NetSarang Computer\6\%s\Sessions" % dir)
 
-                        password = decrypt_string(args.sid, password)
-                    else:
-                        host = cfg["Connection"]["Host"]
-                        port = cfg["Connection"]["Port"]
-                        username = cfg["Connection"]["UserName"]
-                        password = cfg["Connection"]["Password"]
-                        version = cfg["SessionInfo"]["Version"]
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
 
-                        password = decrypt_string(args.sid, password)
+    try:
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
 
-                    print(f"{filepath:=^100}")
-                    print('%-10s : %s' % ('Host', host))
-                    print('%-10s : %s' % ('Port', port))
-                    print('%-10s : %s' % ('Version', version))
-                    print('%-10s : %s' % ('UserName', username))
-                    print('%-10s : %s' % ('Password', password))
-                except Exception as e:
-                    print(f"{filepath:=^100}\nError:{e}")
+    return False
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="xsh, xfp password decrypt")
-    group = parser.add_mutually_exclusive_group()
+    group = parser.add_mutually_exclusive_group(required=False)
     group.add_argument("-e", "--encrypt", default=False,
                        help="encrypt password", action="store_true")
     group.add_argument("-d", "--decrypt", default=True,
                        help="decrypt encrypted password", action="store_true")
+    parser.add_argument("-f", "--ftp", default=False,
+                        help="it is xftp, or xshell.", action="store_true")
     parser.add_argument("-u", "--username", default="", type=str,
                         help="user `whoami /user` in command.")
+    parser.add_argument("-m", "--master_pwd", default="", type=str,
+                        help="user's master password.")
     parser.add_argument("-s", "--sid", default="", type=str,
                         help="SID `whoami /user` in command.")
     parser.add_argument("-v", "--version", default="", type=str,
                         help="xsh or xfp version")
     parser.add_argument("-k", "--key", default="", nargs='?',
-                        help="the path of sessions directory or file, or password or other key")
+                        help="the path of sessions directory or file of xsh or xfp, or password or other key")
 
     args = parser.parse_args()
-    print(args)
-    if not args.sid:
-        args.sid = ConvertSidToStringSid(LookupAccountName(GetComputerName(), GetUserName())[0])
-    if not args.key:
-        # args.key = os.path.join(os.environ["USERPROFILE"], r"Documents\NetSarang Computer\6")
-        args.key = os.path.join(os.environ["USERPROFILE"], r"Documents\NetSarang\Xshell\Sessions")
 
-    if not os.path.isdir(args.key):
-        r = decrypt_string(args.sid, args.key)
-        if r:
-            print(r)
+    #print(args)
+
+    if args.encrypt:
+        IS_DECRYPT = False
+    if args.sid:
+        SID = args.sid
+    if args.username:
+        USERNAME = args.username
+    if args.master_pwd:
+        MASTER_PWD = args.master_pwd
+    if args.ftp:
+        IS_XSH = False
+    if is_number(args.version):
+        VERSION = args.version
+    if args.key:
+        KEY = args.key
+
+    if not args.key and (is_number(args.version) or args.ftp):
+        setDefaultSessionDirByVer()
+
+    if IS_DECRYPT:
+        if os.path.isdir(KEY):
+            decrypt_dir()
+        elif os.path.isfile(KEY):
+            decrypt_file(KEY)
+        else:
+            decrypt_string(KEY)
+    else:
+        encrypt_string(KEY)
